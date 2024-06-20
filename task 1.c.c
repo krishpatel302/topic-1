99ff8e3089dce1d37fa5a03aa9c0e994901a8289
krishpatel302 <krishbhanderi7@gmail.com>
2024-06-19 23:39:57 -0700

    Add files via upload

---
+++ task 1.c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+
+{
+	printf(" name :- krish\n");
+	printf(" father's name :- vinodbhai\n");
+	printf(" surname :- bhanderi\n");
+	printf(" age :- 18\n");
+	printf(" school :- s.p.g\n");
+}
